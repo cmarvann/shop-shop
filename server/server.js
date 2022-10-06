8315d38d5d158bfdb5edb3f791f0c9f5aadd8d88
cmarvann <covingtonmarvann@gmail.com>
2022-10-06 16:09:00 -0400

    reinstall packagejson

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-server.applyMiddleware({ app });
+// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -30,9 +30,17 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+};
+
+startApolloServer(typeDefs, resolvers);
